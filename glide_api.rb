e5559b236ab1f13d30572ac0fde7d24aaa55a984
IMP1 <IMP1@mail.com>
2017-03-24 16:09:05 +0000

    changed parametere procedure

--- glide_api.rb
+++ glide_api.rb
@@ -1,20 +1,22 @@
+require_relative 'workflow'
+
 module GlideAPI
 
     def self.handle_command(command, *args)
         puts "[GlideAPI] Recieved command '#{command}'."
         case command
         when "workflows"
-            workflow_command(args[0], args[1..-1])
+            workflow_command(args[0], *args[1..-1])
         else
 
         end
     end
 
-    def self.workflow_command(command, args)
+    def self.workflow_command(command, *args)
         puts "[GlideAPI] Recieved Workflow command '#{command}'."
         case command
         when "create"
-            p args[0]
+            Workflow.create_workflow(args[0])
         else
 
         end
