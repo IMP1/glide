ab81a2a612fb7755564a47aff3579262a329be58
IMP1 <IMP1@mail.com>
2017-03-24 15:49:30 +0000

    minorly 'handles' commands

--- glide_api.rb
+++ glide_api.rb
@@ -1,7 +1,23 @@
 module GlideAPI
 
     def self.handle_command(command, *args)
+        puts "[GlideAPI] Recieved command '#{command}'."
+        case command
+        when "workflows"
+            workflow_command(args[0], args[1..-1])
+        else
 
+        end
+    end
+
+    def self.workflow_command(command, args)
+        puts "[GlideAPI] Recieved Workflow command '#{command}'."
+        case command
+        when "create"
+            p args[0]
+        else
+
+        end
     end
 
 end
