af72ac88f91ee4f8ce37ee5d2809b37e3b0dd6f2
IMP1 <IMP1@mail.com>
2017-03-27 15:40:42 +0100

    added importance and global depth

--- log.rb
+++ log.rb
@@ -1,16 +1,18 @@
 class Logger
 
+    NONE        = 0
     ERROR       = 1
     WARNING     = 2
     INFORMATION = 3
     DEBUG       = 4
 
     attr_reader :last_message
+    
+    @@depth = 0
 
-    def initialize(source, output=STDOUT)
+    def initialize(source, output=$stdout)
         @source = source
         @out = output
-        @depth = 0
         @padding = 4
         @last_message = ""
         @importance_level = DEBUG
@@ -20,21 +22,24 @@ class Logger
         @importance_level = level
     end
 
-    def push(message)
-        @out.puts (" " * padding * depth) + message
-        @depth += 1
-        @last_message = @message
+    def push(message, importance=INFORMATION)
+        log_message(message, importance)
+        @@depth += 1
     end
 
-    def pop(message)
-        @depth = [@depth - 1, 0].max
-        @outs.puts (" " * padding * depth) + message
-        @last_message = @message
+    def pop(message, importance=INFORMATION)
+        @@depth = [@@depth - 1, 0].max
+        log_message(message, importance)
     end
 
-    def log(message)
-       @outs.puts (" " * padding * depth) + message 
-       @last_message = @message
+    def log(message, importance=INFORMATION)
+        log_message(message, importance)
+    end
+
+    def log_message(message, importance)
+        return if importance > @importance_level
+        @out.puts "[#{@source}]" + (" " * @padding * @@depth) + message 
+        @last_message = message
     end
 
 end
