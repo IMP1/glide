531189620cef230835b8d2e2f16c691b1003c936
IMP1 <IMP1@mail.com>
2017-03-28 14:42:58 +0100

    added more structure to workflow commands

--- workflow.rb
+++ workflow.rb
@@ -1,16 +1,80 @@
-class Workflow
+module Workflow
 
     @@logger = Logger.new("Workflow")
 
-    def initialize(id=nil)
-        if id.is_a? Hash
-            @@logger.push("Creating workflow...")
-            @@logger.pop("Workflow #{} created.")
-        elsif id.is_a? Integer
-            @@logger.log("Retrieving workflow #{id}...")
-        else
-            @@logger.log("Invalid ID or setup hash '#{id.inspect}'.", Logger::ERROR)
+    def self.create(details)
+        @@logger.push("Creating workflow...")
+        begin
+            id = 0 # TODO: remove this and have it obtained from the database.
+            # TODO: add workflow to database.
+            @@logger.pop("Workflow #{id} created.")
+            return id
+        rescue Exception => e
+            @@logger.pop(e.to_s, Logger::ERROR)
+            return nil
         end
+    end
+
+    def self.all
+        @@logger.log("Retrieving all workflows...")
+        begin
+            all_workflows = [] # TODO: remove this and have it obtained from the database.
+            # TODO: add workflow to database.
+            return all_workflows
+        rescue Exception => e
+            @@logger.log(e.to_s, Logger::ERROR)
+            return nil
+        end
+    end
+
+    def self.read(id)
+        return all if id.nil?
+        @@logger.log("Retrieving workflow #{id}...")
+        begin
+            if exists?(id)
+                # TODO: update workflow
+                return true
+            else
+                return nil
+            end
+        rescue Exception => e
+            @@logger.log(e.to_s, Logger::ERROR)
+            return nil
+        end
+    end
+
+    def self.update(id, details)
+        @@logger.log("Updating workflow #{id}...")
+        begin
+            if exists?(id)
+                # TODO: update workflow
+                return true
+            else
+                return false
+            end
+        rescue Exception => e
+            @@logger.log(e.to_s, Logger::ERROR)
+            return nil
+        end
+    end
+
+    def self.delete(id)
+        @@logger.log("Deleting workflow #{id}...")
+        begin
+            if exists?(id)
+                # TODO: delete workflow
+                return true
+            else
+                return false
+            end
+        rescue Exception => e
+            @@logger.log(e.to_s, Logger::ERROR)
+            return nil
+        end
+    end
+
+    def self.exists?(id)
+
     end
 
 end
