265e092e0caae8988a0b17318555b9e9e8990b59
IMP1 <IMP1@mail.com>
2017-03-28 14:42:39 +0100

    reshaped the command handling

--- glide_command_handler.rb
+++ glide_command_handler.rb
@@ -4,21 +4,46 @@ module GlideCommandHandler
 
     @logger = Logger.new("GlideAPI")
 
-    def self.handle_command(command, *args)
-        @logger.log "Recieved command '#{command}'."
-        case command
-        when "workflows"
-            workflow_command(args[0], *args[1..-1])
+    def self.create(datatype, options)
+        case datatype
+        when "workflow"
+            workflow_details = args[0]
+            workflow = Workflow.create(workflow_details)
+            return workflow.id
         else
 
         end
     end
 
-    def self.workflow_command(command, *args)
-        @logger.log "Recieved Workflow command '#{command}'."
-        case command
-        when "create"
-            Workflow.create_workflow(args[0])
+    def self.read(datatype, id)
+        case datatype
+        when "workflow"
+            workflow_id = args[0]
+            workflow = Workflow.read(workflow_id)
+            return workflow
+        else
+
+        end
+    end
+
+    def self.update(datatype, id, options)
+        case datatype
+        when "workflow"
+            workflow_id = args[0]
+            workflow_details = args[1]
+            success = Workflow.update(workflow_id, workflow_details)
+            return success
+        else
+
+        end
+    end
+
+    def self.delete(datatype, id)
+        case datatype
+        when "workflow"
+            workflow_id = args[0]
+            success = Workflow.delete(workflow_id)
+            return success
         else
 
         end
