134b1eafbfc4baad2065cbdb6aac204f4e044ba2
IMP1 <IMP1@mail.com>
2017-03-29 10:04:27 +0100

    added error logging for invalid datatypes

--- glide_command_handler.rb
+++ glide_command_handler.rb
@@ -6,41 +6,49 @@ module GlideCommandHandler
 
     def self.create(datatype, options)
         case datatype
-        when "workflow"
-            workflow = Workflow.create(options)
-            return workflow.id
+        when "workflows"
+            workflow_id = Workflow.create(options)
+            return workflow_id
         else
-
+            error_message = "'#{datatype}' not recognised. Cannot create."
+            @logger.log(error_message, Logger::ERROR)
+            return nil, error_message
         end
     end
 
     def self.read(datatype, id)
         case datatype
-        when "workflow"
+        when "workflows"
             workflow = Workflow.read(id)
             return workflow, nil
         else
-            return nil, "'#{datatype}' not recognised."
+            error_message = "'#{datatype}' not recognised. Cannot read."
+            @logger.log(error_message, Logger::ERROR)
+            return nil, error_message
         end
     end
 
     def self.update(datatype, id, options)
         case datatype
-        when "workflow"
+        when "workflows"
             success = Workflow.update(id, options)
             return success
         else
-
+            error_message = "'#{datatype}' not recognised. Cannot update."
+            @logger.log(error_message, Logger::ERROR)
+            return nil, error_message
         end
     end
 
     def self.delete(datatype, id)
         case datatype
-        when "workflow"
+        when "workflows"
             success = Workflow.delete(id)
             return success
         else
-
+            error_message = "'#{datatype}' not recognised. Cannot delete."
+            @logger.log(error_message, Logger::ERROR)
+            return nil, error_message
         end
     end
 
