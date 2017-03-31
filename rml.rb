d34c18dffa82d14ae36f62208889e4855a33d838
IMP1 <IMP1@mail.com>
2017-03-31 12:33:06 +0100

    tags that go across multiple lines should now work

--- rml.rb
+++ rml.rb
@@ -89,9 +89,10 @@ class RMLParser
 
         @string.gsub!(/\n\s*\n?/m, "\n")
 
+        unfinished_tags = []
         lines = @string.split("\n")
         depth = 0
-        lines.map! do |line|
+        lines = lines.map.with_index do |line, line_number|
             padding = " " * (depth * 4)
             if opening_tags.size > 0 && line.include?("<#{opening_tags.first}")
                 tag_name = opening_tags.delete_at(0)
@@ -103,7 +104,19 @@ class RMLParser
                 depth -= 1
                 padding = " " * (depth * 4)
             end
+            if line.count('<') > line.count('>')
+                unfinished_tags.push({ :tag => tag_name, :line => line_number })
+            end
             padding + line
+        end
+        unfinished_tags.each do |a|
+            i = a[:tag].length + 1
+            line_number = a[:line] + 1
+            loop do
+                lines[line_number] = (" " * i) + lines[line_number][3..-1]
+                break if lines[line_number].include? ">"
+                line_number += 1
+            end
         end
         @string = lines.join("\n")
     end
